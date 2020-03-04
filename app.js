db07608580cae12da003084963f45d6f786b0f3f
Martin Komischke <martin.komischke@quentic.com>
2020-03-04 16:33:53 +0100

    use CSRF logic

--- app.js
+++ app.js
@@ -1,9 +1,29 @@
 var express = require('express');
-var app = express();
+var cookieParser = require('cookie-parser')    //for cookie parsing
+var csrf = require('csurf')    //csrf module
+var bodyParser = require('body-parser')    //for body parsing
+
+// setup route middlewares
+var csrfProtection = csrf({ cookie: true })
+var parseForm = bodyParser.urlencoded({ extended: false })
+
+// create express app
+var app = express()
+
+// parse cookies
+app.use(cookieParser())
 
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
+app.get('/form', csrfProtection, function(req, res) {
+  // generate and pass the csrfToken to the view
+  res.render('send', { csrfToken: req.csrfToken() })
+})
+
+app.post('/process', parseForm, csrfProtection, function(req, res) {
+  res.send('data is being processed')
+})
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
